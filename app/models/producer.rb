d038a979ccbace58b78ef3eac5dfb79afc1275c5
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2020-11-30 17:06:15 +0900

    ActiveHashの導入

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,6 +1,11 @@
 class Producer < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+         
+  validates :genre_id, numericality: { other_than: 1 } 
 end
