3d5e48e2348f969bab4cf1d726707628c9e1fc07
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2020-12-01 10:21:52 +0900

    ログイン機能修正

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,11 +1,12 @@
 class Producer < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+  has_one_attached :image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
          
-  validates :genre_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
 end
