57c67b9a8254944edd815699b383d990b7ddc9ca
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2021-01-15 14:48:07 +0900

    購入機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :kana_family_name, :kana_first_name, :prefecture_id, :municipality, :address, :building, :phone_number, :birthday, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :kana_family_name, :kana_first_name, :prefecture_id, :municipality, :address, :building, :phone_number, :birthday, :image, :nickname])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :producer, optional: true
   belongs_to :customer, optional: true
+  has_one :item_order
+  has_one :order
   has_one_attached :image
 end
